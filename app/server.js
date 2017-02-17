222e1659965cd7fbd184f5ac97f2f46674955dc3
catalindumitrache76 <catalindumitrache76@yahoo.es>
2017-02-17 21:32:04 +0100

    First commit

--- app/server.js
+++ app/server.js
@@ -1,11 +1,18 @@
 'use strict';
 
 // Constants
-const PORT = 80;
+const PORT = 8888;
 var http=require("http");
-
+var url = require ('url');
+console.log("Server running on port "+PORT);
 http.createServer(function(request,response){
-	response.writeHead(200,{"Content-Type":"HTML"});
-	response.write("Hello World2!");
+	var pathname = url.parse(request.url).pathname;
+	
+	console.log("Request for " + pathname + " received.");
+	console.log("%j", request);
+	
+	response.writeHead(200,{'Content-Type':'html'});
+	response.write("Hello World!");
 	response.end();
+	
 }).listen(PORT);
